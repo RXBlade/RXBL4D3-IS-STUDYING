8780367da17ce0ddcef70757aaec69c6858728c6
RXBLADE <andrey_pascuta@yahoo.com>
2022-04-30 16:10:13 +0000

    Added Functions and Classes

---
+++ PYTHON/10. Classes & Objects/howToAccessData.py
@@ -0,0 +1,11 @@
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        return "This is a message inside the class."
+
+myobjectx = MyClass()
+
+print(myobjectx.variable)
+
+print(myobjectx.function())
